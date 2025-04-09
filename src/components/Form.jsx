1a1c7c9b80c83fe8c76dc242dfac08a2ea4714f1
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-09 15:40:26 +0200

    feat: add delete article function

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,6 +2,6 @@ export default function Form({ addArticle, setNewArticle, newArticle }) {
 
     return <form onSubmit={addArticle}>
         <input type="text" placeholder="Insert article title" onChange={e => { setNewArticle(e.target.value) }}/>
-        <button>Add</button>
+        <button className="add-btn">Add</button>
     </form>
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,9 @@
 import { useState } from 'react';
 import Form from './Form';
 
+// react icon
+import { AiFillDelete } from "react-icons/ai";
+
 export default function Main({ articles }) {
 
     // articles list
@@ -21,9 +24,23 @@ export default function Main({ articles }) {
 
     }
 
+    const delArticle = (index) => {
+
+        const updateArticle = articlesList.filter((art, i) => {
+
+            return index !== i;
+        })
+
+        setArticles(updateArticle);
+    }
+
     return <div className='main-box'>
         <ul>
-            {articlesList.map((art, i) => <li key={i}>{art}</li>)}
+            {articlesList.map((art, i) => 
+                <li key={i}>
+                    {art} 
+                    <button onClick={() => delArticle(i)}><AiFillDelete /></button>
+                </li>)}
         </ul>
 
         <Form addArticle={addArticle} setNewArticle={setNewArticle} newArticle={newArticle} />
